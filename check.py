3455febe359e94063670ca825e56c9e51c37a438
Prakarsh Anand <prakarsh@Prakarshs-MacBook-Pro.local>
2020-01-16 17:06:39 +0530

    realmess

---
+++ check.py
@@ -0,0 +1,93 @@
+from datetime import timedelta
+
+
+import airflow
+from airflow import DAG
+from airflow import hooks, settings
+from airflow.contrib.operators.emr_create_job_flow_operator import EmrCreateJobFlowOperator
+from airflow.contrib.operators.emr_add_steps_operator import EmrAddStepsOperator
+from airflow.contrib.sensors.emr_step_sensor import EmrStepSensor
+from airflow.contrib.operators.emr_terminate_job_flow_operator import EmrTerminateJobFlowOperator
+
+default_args = {
+    'owner': 'Airflow',
+    'depends_on_past': False,
+    'start_date': datetime(2020, 1, 13, 11, 39, 0),
+    'email': ['prakarsh2512@gmail.com'],
+    'email_on_failure': False,
+    'email_on_retry': True,
+    'retries': 1,
+    'retry_delay': timedelta(minutes=5),
+    # 'queue': 'bash_queue',
+    # 'pool': 'backfill',
+    # 'priority_weight': 10,
+    # 'end_date': datetime(2016, 1, 1),
+}
+ds= "Prakarsh"
+
+HIVE_TEST_STEPS = [
+    {
+        'Name': 'test step',
+        'ActionOnFailure': 'CONTINUE',
+        'HadoopJarStep': {
+            'Jar': 'command-runner.jar',
+            'Args': [
+                '-e',
+                'msck repair table dataplatform.booking',
+                '',
+                '',
+                '-d',
+                ''
+            ]
+        }
+    }
+]
+
+
+dag = DAG(
+    'check_emr_steps',
+    default_args=default_args,
+    dagrun_timeout=timedelta(hours=2),
+    #schedule_interval='0 3 * * *'
+    schedule_interval=timedelta(minutes=5)
+)
+
+
+
+#
+# cluster_creator = EmrCreateJobFlowOperator(
+#     task_id='create_job_flow',
+#     job_flow_overrides=JOB_FLOW_OVERRIDES,
+#     aws_conn_id='aws_default',
+#     emr_conn_id='emr_default',
+#     dag=dag
+# )
+
+step_adder = EmrAddStepsOperator(
+    task_id='add_steps',
+    job_flow_id="j-3VQJPBNHI0US4",
+    aws_conn_id='aws_default',
+    steps=HIVE_TEST_STEPS,
+    dag=dag
+)
+
+step_checker = EmrStepSensor(
+    task_id='watch_step',
+    job_flow_id="j-3VQJPBNHI0US4",
+    step_id="{{ task_instance.xcom_pull('add_steps', key='return_value')[0] }}",
+    aws_conn_id='aws_default',
+    dag=dag
+)
+
+# cluster_remover = EmrTerminateJobFlowOperator(
+#     task_id='remove_cluster',
+#     job_flow_id="{{ task_instance.xcom_pull('create_job_flow', key='return_value') }}",
+#     aws_conn_id='aws_default',
+#     dag=dag
+# )
+
+#cluster_creator.set_downstream(step_adder)
+#step_adder.set_downstream(step_checker)
+#step_checker.set_downstream(cluster_remover)
+step_adder.set_downstream(step_checker)
+
