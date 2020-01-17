56e401592eefc863da747e10ce2aec60cc580e08
Prakarsh Anand <prakarsh@Prakarshs-MacBook-Pro.local>
2020-01-17 13:52:38 +0530

    lets try

--- check.py
+++ check.py
@@ -12,7 +12,7 @@ local_tz = pendulum.timezone("Asia/Kolkata")
 default_args = {
     'owner': 'Airflow',
     'depends_on_past': False,
-    'start_date': datetime(2020, 1, 17, 11, 20, 0,tzinfo=local_tz),
+    'start_date': datetime(2020, 1, 17, 11, 20, 0, tzinfo=local_tz),
     'email': ['prakarsh2512@gmail.com'],
     'email_on_failure': False,
     'email_on_retry': True,
@@ -22,69 +22,63 @@ default_args = {
     # 'priority_weight': 10,
     # 'end_date': datetime(2016, 1, 1),
 }
-ds= "Prakarsh"
-
-HIVE_TEST_STEPS = [
-    {
-        'Name': 'test step',
-        'ActionOnFailure': 'CONTINUE',
-        'HadoopJarStep': {
-            'Jar': 'command-runner.jar',
-            'Args': [
-                'hive',
-                '-e',
-                'msck repair table dataplatform.booking'
-            ]
-        }
-    }
-]
-
 
 dag = DAG(
-    'prakarsh_hive',
-    default_args=default_args,
+    'check_emr_steps',
     catchup=False,
+    default_args=default_args,
     dagrun_timeout=timedelta(hours=2),
-    #schedule_interval='0 3 * * *'
+    # schedule_interval='0 3 * * *'
     schedule_interval=timedelta(minutes=5)
 )
+step_adder=[]
+step_checker=[]
+def poke():
+    hook = hooks.S3_hook.S3Hook(aws_conn_id='aws_s3')
+    job_flow_id = "j-2ASQREUMPJ0Y7"
+    aws_conn_id = 'aws_emr'
+    st=hook.read_key(key='prod_deployment/conf/athena_all_tables', bucket_name='bounce-data-platform')
+    loop=st.split("\n")
+    for i in range(len(loop)):
+        steps=[
+            {
+                'Name': 'test step',
+                'ActionOnFailure': 'CONTINUE',
+                'HadoopJarStep': {
+                    'Jar': 'command-runner.jar',
+                    'Args': [
+                        'hive',
+                        '-e',
+                        loop[i]
+                    ]
+                }
+            }
+        ]
+        step_addr = EmrAddStepsOperator(
+            task_id='add_steps '+str(i),
+            job_flow_id="j-2ASQREUMPJ0Y7",
+            aws_conn_id='aws_emr',
+            steps=steps,
+            dag=dag
+        )
+        step_adder.append(step_addr)
+        step_checkr = EmrStepSensor(
+            task_id='watch_step'+str(i),
+            job_flow_id="j-2ASQREUMPJ0Y7",
+            step_id="{{ task_instance.xcom_pull('add_steps', key='return_value')[0] }}",
+            aws_conn_id='aws_emr',
+            dag=dag
+        )
+        step_checker.append(step_checkr)
 
+def execute():
+    chain(step_adder,step_checker)
 
+def main():
+    poke()
+    execute()
 
-#
-# cluster_creator = EmrCreateJobFlowOperator(
-#     task_id='create_job_flow',
-#     job_flow_overrides=JOB_FLOW_OVERRIDES,
-#     aws_conn_id='aws_default',
-#     emr_conn_id='emr_default',
-#     dag=dag
-# )
+if __name__=="__ main__":
+    main()
 
-step_adder = EmrAddStepsOperator(
-    task_id='add_steps',
-    job_flow_id="j-2ASQREUMPJ0Y7",
-    aws_conn_id='aws_emr',
-    steps=HIVE_TEST_STEPS,
-    dag=dag
-)
-
-step_checker = EmrStepSensor(
-    task_id='watch_step',
-    job_flow_id="j-2ASQREUMPJ0Y7",
-    step_id="{{ task_instance.xcom_pull('add_steps', key='return_value')[0] }}",
-    aws_conn_id='aws_emr',
-    dag=dag
-)
-
-# cluster_remover = EmrTerminateJobFlowOperator(
-#     task_id='remove_cluster',
-#     job_flow_id="{{ task_instance.xcom_pull('create_job_flow', key='return_value') }}",
-#     aws_conn_id='aws_default',
-#     dag=dag
-# )
-
-#cluster_creator.set_downstream(step_adder)
-#step_adder.set_downstream(step_checker)
-#step_checker.set_downstream(cluster_remover)
-step_adder.set_downstream(step_checker)
 
